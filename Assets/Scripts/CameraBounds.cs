bbe910aa030d328d2d87002e565e32b49a3a4c76
rayriver <Messanger.6085@gmail.com>
2016-12-11 18:42:27 +0900

    camera bounds

---
+++ Assets/Scripts/CameraBounds.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBounds : MonoBehaviour
+{
+    void Awake()
+    {
+        var collider = GetComponent<BoxCollider2D>();
+        if (collider != null)
+        {
+            Camera.main.GetComponent<Camera2D>().SetBounds(collider.bounds);
+        }
+    }
+}
